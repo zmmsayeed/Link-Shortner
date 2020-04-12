c38a855870a9ed1230531dec10c28bf2b6993e4c
Zimam Sayeed (Pesonal) <zmmsayeed@gmail.com>
2020-04-12 22:13:32 +0530

    Changed design of dashboard

--- ls-client/src/components/Dashboard/index.js
+++ ls-client/src/components/Dashboard/index.js
@@ -11,10 +11,21 @@ import LinkShortnerReducer from '../../reducer/LinkShortnerReducer';
 // importing redux actions
 import { callApi } from '../../actions';
 
+// importing icons
+import { FaArrowRight } from 'react-icons/fa';
+import { MdClose } from 'react-icons/md';
+
 // importing stylesheet 
 import './style.css';
 
 class Dashboard extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            customDomain: false
+        }
+    }
 
     handleSubmit = (values) => {
         console.log("Values: ", values)
@@ -22,11 +33,23 @@ class Dashboard extends Component {
 
     validate = (values) => {
         const errors = {};
-        if(!values.link) {
+        if (!values.link) {
             errors.link = "Long URL is required!"
         }
 
         return errors
+    }
+
+    customDomain = () => {
+        this.setState({
+            customDomain: true
+        })
+    }
+
+    closeCustomDomain = () => {
+        this.setState({
+            customDomain: false
+        })
     }
 
     render() {
@@ -35,52 +58,74 @@ class Dashboard extends Component {
                 <Navbar />
 
                 <div className="innerContainer p-5 h-100 mx-auto mt-5">
+                    <h1 className="text-center font-weight-bold">SHORTENING YOUR URL?</h1>
+                    <h4 className="text-center mb-5">Sure, we will do that at a mouse click!</h4>
                     <Form onSubmit={this.handleSubmit}
                         validate={this.validate}
                         render={({ handleSubmit, submitting, valid, pristine }) => (
                             <form onSubmit={handleSubmit} >
-                                <Field name="link" placeholder="Enter long URL">
-                                    {({ input, meta, placeholder }) => {
-                                        let error = false;
-                                        if (meta.error && meta.visited && !meta.active) {
-                                            error = true;
-                                        }
-                                        else {
-                                            error = false;
-                                        }
-                                        return (
-                                            <div className="form-group">
-                                                <label>Enter Long URL:</label>
-                                                <input {...input} type="text" placeholder={placeholder} className={ error?"form-control is-invalid":"form-control"} />
-                                                <small className={ error?"form-text text-danger":"hidden" }>{meta.error}</small>
-                                            </div>
-                                        )
-                                    }}
-                                </Field>
-
-                                <Field name="token" placeholder="Enter Custom Token (Optional):">
-                                    {({ input, meta, placeholder }) => {
-                                        return (
-
-                                            <div className="form-group mb-5">
-                                                <label>{placeholder}</label>
-                                                <div className="input-group">
-                                                    <div className="input-group-prepend">
-                                                        <div className="input-group-text font-weight-bold">http://someshorturl.com/</div>
+                                <div className="row">
+                                    <div className={this.state.customDomain?"col-md-12":"col-md-11"}>
+                                        <Field name="link" placeholder="http://enter-long-url-here.com">
+                                            {({ input, meta, placeholder }) => {
+                                                let error = false;
+                                                if (meta.error && meta.visited && !meta.active) {
+                                                    error = true;
+                                                }
+                                                else {
+                                                    error = false;
+                                                }
+                                                return (
+                                                    <div className="form-group">
+                                                        <input {...input} type="text" placeholder={placeholder} className={error ? "form-control is-invalid" : "form-control"} />
+                                                        <small className={error ? "form-text text-danger" : "hidden"}>{meta.error}</small>
                                                     </div>
-                                                    <input {...input} type="text" className="form-control" placeholder="customToken" />
-                                                </div>
-                                            </div>
-                                        )
-                                    }}
-                                </Field>
+                                                )
+                                            }}
+                                        </Field>
+                                    </div>
+                                    <div className={this.state.customDomain?"hidden":"col-md-1"}>
+                                        <button type="submit" className="btn btn-info mb-5 generateButton" disabled={submitting || !valid || pristine}>
+                                            <FaArrowRight />
+                                        </button>
+                                    </div>
+                                </div>
 
+                                <p className={this.state.customDomain ? "hidden" : "text-center p-3 font-weight-bold text-light link"} onClick={this.customDomain}>
+                                    Enter Custom Domain
+                                </p>
 
-                                <button type="submit" className="btn btn-info mb-5" disabled={submitting || !valid || pristine}>
-                                    Generate Short URL
-                                </button>
+                                <div className={this.state.customDomain ? "mt-3" : "hidden"}>
+                                    <div className="row">
+                                        <div className="col-md-12">
+                                            <Field name="token" placeholder="Enter Custom Token (Optional):">
+                                                {({ input, meta, placeholder }) => {
+                                                    return (
 
-                                <p className="text-danger"><b>NOTE: </b> Link generated will be valid only for 7 days.</p>
+                                                        <div className="form-group mb-5">
+                                                            <div className="input-group">
+                                                                <div className="input-group-prepend">
+                                                                    <div className="input-group-text font-weight-bold">http://someshorturl.com/</div>
+                                                                </div>
+                                                                <input {...input} type="text" className="form-control" placeholder="customToken" />
+                                                            </div>
+                                                        </div>
+                                                    )
+                                                }}
+                                            </Field>
+                                        </div>
+                                        <div className="col-md-12 text-center">
+                                            <button className="btn btn-danger mb-5 generateButton mr-4" onClick={ this.closeCustomDomain }>
+                                                <MdClose />
+                                            </button>
+                                            <button type="submit" className="btn btn-info mb-5 generateButton" disabled={submitting || !valid || pristine}>
+                                                <FaArrowRight />
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <p className="text-center text-danger p-2"><b>NOTE: </b> Link generated will be valid only for 7 days.</p>
                             </form>
                         )}
                     />
